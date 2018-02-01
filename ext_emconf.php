feefc6a8a4b0637e3e06ca0d8860b3a9da07abb3
Thomas Löffler <loeffler@spooner-web.de>
2018-02-01 17:44:01 +0100

    Add basic ext_emconf and composer data

---
+++ ext_emconf.php
@@ -0,0 +1,16 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Fluid Form Elements',
+    'description' => 'Easy to use form elements for Extbase/Fluid extensions',
+    'category' => 'frontend',
+    'author' => 'Thomas Löffler',
+    'state' => 'stable',
+    'clearCacheOnLoad' => true,
+    'lockType' => '',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-8.7.99',
+        ]
+    ],
+];
