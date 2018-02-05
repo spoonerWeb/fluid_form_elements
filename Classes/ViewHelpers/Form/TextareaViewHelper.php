3d0a7b9e1280dfad297aea144967d722b56f54d1
Thomas Löffler <loeffler@spooner-web.de>
2018-02-05 09:27:15 +0100

    Add Textarea view helper

---
+++ Classes/ViewHelpers/Form/TextareaViewHelper.php
@@ -0,0 +1,26 @@
+<?php
+namespace SpoonerWeb\FluidFormElements\ViewHelpers\Form;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * Class TextareaViewHelper
+ *
+ * @author Thomas Löffler <loeffler@spooner-web.de>
+ */
+class TextareaViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Form\TextareaViewHelper
+{
+    use \SpoonerWeb\FluidFormElements\ViewHelpers\FormFieldTrait;
+
+}
